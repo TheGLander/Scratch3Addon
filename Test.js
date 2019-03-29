abfe8e63983f112358d68403fbb3c6d18d603a89
Lubomir <lubomir.yudenko@gmail.com>
2019-03-29 20:25:05 +0200

    Update Test.js

--- Test.js
+++ Test.js
@@ -1,9 +1,11 @@
+const icon = "https://github.com/TheGLander/Scratch3Addon/blob/master/char-g-md.ico"
 class Test {
     constructor() {}
     getInfo() {
         return{
             id: 'Test',
             name: "G's extension test",
+            menuIconURI: icon
         }
     }
 }
