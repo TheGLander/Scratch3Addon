684a8e0d907ff80ea2141a2cb3482f29a2038366
Lubomir <lubomir.yudenko@gmail.com>
2019-03-29 19:56:23 +0200

    Initial commit

---
+++ Test.js
@@ -0,0 +1,10 @@
+var MyExtension = function () {;
+    id = "test",
+    name = "Test"
+};
+MyExtension.prototype.getInfo = function () {
+    return {
+        
+// Object goes here
+}}
+Scratch.extensions.register(new MyExtension());
